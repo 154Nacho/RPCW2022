62ba99908ec62f062d5751400c65e8ec1fb0e5f3
Pedro Barbosa <49070051+154Nacho@users.noreply.github.com>
2022-02-26 10:14:09 +0000

    generate home pages + movies

--- TPC2/parserHTML.py
+++ TPC2/parserHTML.py
@@ -14,7 +14,7 @@ actorID = 1
 for item in data :
 
      movies[item['title']] = {
-         'actors' : set(x for x in item['cast']),
+         'atores' : set(x for x in item['cast']),
          'ano' : item['year'],
          'genero' : set(x for x in item['genres']),
          'ID' : movieID
@@ -34,4 +34,64 @@ for item in data :
 
 movies = dict(sorted(movies.items(), key = lambda x : x[0]))
 actors = dict(sorted(actors.items(), key = lambda x : x[0]))
-     
+
+def genHomePage() :
+    homePage_path = "./html/index.html"
+    homePage = open(homePage_path,"w")
+    homePage.write("<!DOCTYPE html>\n")
+
+    homePage.write("<html lang=\"en\">\n")
+
+    homePage.write("\t<head>\n")
+
+    homePage.write("\t\t<title>TPC2 - List of Movies and Actors</title>\n")
+    homePage.write("\t\t<meta charset=\"UTF-8\">")
+    homePage.write("\t\t<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">")
+
+    homePage.write("\t</head>\n")
+
+    homePage.write("\t<body>\n")
+
+    homePage.write("\t\t<h1><a href=\"http://localhost:7777/filmes\">Movies</a></h1>")
+    homePage.write("\t\t<h1><a href=\"http://localhost:7777/atores\">Actors</a></h1>")
+
+    homePage.write("\t</body>\n")
+
+    homePage.write("</html>\n")
+
+def genMoviesPage(data) : 
+    moviesPage_path = "./html/filmes.html"
+    moviesPage = open(moviesPage_path,"w")
+    moviesPage.write("<!DOCTYPE html>\n")
+
+    moviesPage.write("<html lang=\"en\">\n")
+
+    moviesPage.write("\t<head>\n")
+
+    moviesPage.write("\t\t<title>Movies</title>\n")
+    moviesPage.write("\t\t<meta charset=\"UTF-8\">")
+    moviesPage.write("\t\t<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">")
+
+    moviesPage.write("\t</head>\n")
+
+    moviesPage.write("\t<body>\n")
+
+    moviesPage.write("\t\t<div class=\"w3-bar w3-center\">\n")
+    moviesPage.write("\t\t\t<h1>Movies</h1>\n")
+    moviesPage.write("\t\t</div>\n")
+
+    moviesPage.write("\t\t<div class=\"w3-container w3-margin-left w3-text-red\">\n")
+    moviesPage.write("\t\t\t<p><a href=\"http://localhost:7777\"><u>Back</u></a><p>\n")
+    moviesPage.write("\t\t</div>")
+
+    moviesPage.write("\t\t<div class=\"w3-container w3-margin-left\">\n")
+    moviesPage.write("\t\t\t<ol>\n")
+
+    for item,values in movies.items() :
+        moviesPage.write("\t\t\t<li><a href=\"http://localhost:7777/filmes/f{values['ID']}\">{item}</a></li>")
+
+    moviesPage.write("\t\t\t<\ol>\n")
+
+    moviesPage.write("\t</body>\n")
+
+    moviesPage.write("</html>\n")
